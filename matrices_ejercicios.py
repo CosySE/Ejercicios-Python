f2c07ecb1d1eee150023dd68c41417f021cfc6ab
DayiTecnologia <79483122+CosySE@users.noreply.github.com>
2022-08-10 17:12:39 -0400

    Add files via upload

--- matrices_ejercicios.py
+++ matrices_ejercicios.py
@@ -10,19 +10,65 @@ def mostrar (elemento):
         print(x)
 
 #mostrar(matriz)
+#[1, 0, 0, 0, 0, 0]
+#0, 1, 0, 0, 0, 0]
+#[0, 0, 1, 0, 0, 0]
+#[0, 0, 0, 1, 0, 0]
+#[0, 0, 0, 0, 1, 0]
+#[0, 0, 0, 0, 0, 1]
+#***************************************************
+# pos_actual = -1
+# for x in matriz:
+#     pos_actual += 1
+#     for j in range(len(matriz)-1):
+#         x[pos_actual] = 1
 
-pos_actual = -1
+# mostrar(matriz)
+#**********************************************************
+#[0, 0, 0, 0, 0, 1]
+#[0, 0, 0, 0, 1, 0]
+#[0, 0, 0, 1, 0, 0]
+#[0, 0, 1, 0, 0, 0]
+#[0, 1, 0, 0, 0, 0]
+#[1, 0, 0, 0, 0, 0]
+# pos_actual = 0
+# for x in matriz:
+#     pos_actual += -1
+#     for j in range(len(matriz)-1):
+#         x[pos_actual] = 1
+
+# mostrar(matriz)
+#***********************************************************
+#[1, 0, 0, 0, 0, 0]
+#[1, 1, 0, 0, 0, 0]
+#[1, 1, 1, 0, 0, 0]
+#[1, 1, 1, 1, 0, 0]
+#[1, 1, 1, 1, 1, 0]
+#[1, 1, 1, 1, 1, 1]
+# cont =0
+# lista=[]
+
+# for x in matriz:
+#     lista.append(cont)
+#     #print(lista)
+#     for j in range(len(lista)):
+#         x[j] = 1
+#     cont += 1
+# mostrar(matriz)
+#**************************************************
+#[0, 0, 0, 0, 0, 1]
+#[0, 0, 0, 0, 1, 1]
+#[0, 0, 0, 1, 1, 1]
+#[0, 0, 1, 1, 1, 1]
+#[0, 1, 1, 1, 1, 1]
+#[1, 1, 1, 1, 1, 1]
+cont =0
+lista=[0]
+
 for x in matriz:
-    pos_actual += 1
-    for j in range(len(matriz)-1):
-        x[pos_actual] = 1
-
-# mostrar(matriz)ckl
-
-pos_actual = 0
-for x in matriz:
-    pos_actual += -1
-    for j in range(len(matriz)-1):
-        x[pos_actual] = 1
-
+    lista.append(cont)
+    #print(lista)
+    for j in range(1,len(lista)):
+        x[-j] = 1
+    cont += -1
 mostrar(matriz)
